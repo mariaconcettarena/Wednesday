cd082a7bc2e582999498514d324890392f1f58b8
mariaconcettarena <mariaconcettaarena@hotmail.it>
2024-02-12 17:52:37 +0100

    aggiunto bottoncino carino

--- Wednesday/Wednesday/Home.swift
+++ Wednesday/Wednesday/Home.swift
@@ -8,24 +8,44 @@
 import Foundation
 import SwiftUI
 
+//variabili per i colori
+let resedaGreen = Color(red: 113/255, green: 119/255, blue: 68/255)
+
 struct Home: View {
+    @State private var isShowingNextPage = false
+
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Home")
-                
-                NavigationLink(destination: Scanning()) {
-                    Text("Go to Scanning")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+       
+            NavigationView {
+              
+                VStack {
+                    Text("Home")
+                    
+                    // Bottone personalizzato
+                    Button(action: {
+                        // Azione quando il bottone viene premuto
+                        isShowingNextPage = true
+                    }) {
+                        Image(systemName: "barcode.viewfinder")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .padding()
+                            .foregroundColor(resedaGreen)
+                            .cornerRadius(10)
+                            .font(.title)
+                    }
+                    .padding()
+                    
+                    
+                    // Navigazione verso la pagina Scanning()
+                    NavigationLink("", destination: Scanning(), isActive: $isShowingNextPage)
+                        .hidden()
                 }
-            }
-            .navigationTitle("Home")
-        }
-    }
+                .navigationTitle("Home")
+            
+        }}
 }
+
 
 
 #Preview {

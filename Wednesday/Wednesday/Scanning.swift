7ca1bb53f025ef2a24eff63553dbc38559d53807
mariaconcettarena <mariaconcettaarena@hotmail.it>
2024-02-16 17:35:28 +0100

    FUNZIONA LETTURA DATABASE

--- Wednesday/Wednesday/Scanning.swift
+++ Wednesday/Wednesday/Scanning.swift
@@ -8,15 +8,46 @@
 import Foundation
 import SwiftUI
 import AVFoundation
+import Combine
+
+
+struct Product: Codable {
+    let barcode: String
+    let company: String
+    let description: String
+    //let image: String
+    let category: String
+    let country: String
+    let standard: Bool
+    let others: String
+}
+
 
 struct Scanning: View {
     @State private var isShowingScanner = false
     @State private var scannedCode: String?
+    @State private var product: Product?
+    
     
     var body: some View {
         VStack {
             if let code = scannedCode {
-                Text("Codice a barre scansionato: \(code)")
+                
+                if let data = product {
+                    Text("BARCODE: \(data.barcode)")
+                    Text("Company: \(data.company)")
+                    Text("Description: \(data.description)")
+                    Text("Category: \(data.category)")
+                    Text("Country: \(data.country)")
+                    Text("IsCrueltyFree?: \(data.standard == true ? "Yes" : data.standard == false ? "No" : "Unknown")")
+                    Text("Others: \(data.others)")
+
+                    
+                } else {
+                    Text("Data not found")
+                }
+                
+                
             } else {
                 Button(action: {
                     isShowingScanner = true
@@ -33,7 +64,43 @@ struct Scanning: View {
         .sheet(isPresented: $isShowingScanner) {
             ScannerView(scannedCode: $scannedCode)
         }
+        .onReceive(Just(scannedCode)) { code in
+                    guard let code = code else { return }
+                    fetchDataFromURL(barcode: code)
+                }
     }
+    
+    
+    // Funzione per ottenere i dati dall'URL
+   private func fetchDataFromURL(barcode: String) {
+        let baseURL = "https://myapisrv.obbar.it/api/Product/getProductByBarcode?barcode="
+        
+        guard let url = URL(string: baseURL + barcode) else {
+            print("URL non valido")
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Errore durante il recupero dei dati:", error?.localizedDescription ?? "Errore sconosciuto")
+                return
+            }
+
+            do {
+                // Decodifica dei dati JSON nella struttura Product e aggiornamento della proprietà @State
+                let decodedData = try JSONDecoder().decode(Product.self, from: data)
+                DispatchQueue.main.async {
+                    self.product = decodedData
+                }
+            } catch {
+                print("Errore durante la decodifica dei dati:", error)
+            }
+        }
+
+        task.resume()
+    }
+    
+ 
 }
 
 struct ScannerView: UIViewControllerRepresentable {
@@ -115,24 +182,12 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
 }
 
 
+
+
+
+
 #Preview {
     Scanning()
 }
 
-/*
-// Bottone SCANNING
-Button(action: {
-    
-    isShowingNextPage = true
-    
-}) {
-    Image(systemName: "barcode.viewfinder")
-        .resizable()
-        .frame(width: 70, height: 70)
-        .padding()
-       // .foregroundColor(resedaGreen)
-        .cornerRadius(10)
-        .font(.title)
-}
-.padding()
-*/
+

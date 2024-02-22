638e9ada719d2f168757397a0da42f8a505a1f0d
mariaconcettarena <mariaconcettaarena@hotmail.it>
2024-02-22 17:50:37 +0100

    aggiunta cardscan, da aggiustare l'impaginazione; risolto il problema dello scan "not found""

--- Wednesday/Wednesday/Scanning.swift
+++ Wednesday/Wednesday/Scanning.swift
@@ -31,12 +31,15 @@ struct Scanning: View {
     @State private var scannedCode: String?
     
     
-    @State var product: Product?
+    @State public var product: Product?
     @Binding public var chronology: [Product]
     @Binding public var favourites: [Product]
     
     
     @State private var isShowingCard = false
+    
+    
+    
     
     
     var body: some View {
@@ -58,24 +61,20 @@ struct Scanning: View {
             
             VStack {
                 
-                if let code = scannedCode {
+                if scannedCode != nil {
                     
-                    if let data = product {
-                        Text("BARCODE: \(data.barcode)")
-                        Text("Name: \(data.name)")
-                        Text("Company: \(data.company)")
-                        Text("Description: \(data.description)")
-                        if let decodedImage = self.decodeBase64ToImage(base64String: data.image) {
-                            Image(uiImage: decodedImage)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 200, height: 200)
-                                .padding()
-                        }
-                        Text("Category: \(data.category)")
-                        Text("Country: \(data.country)")
-                        Text("IsCrueltyFree?: \(data.isCrueltyFree == true ? "Yes" : data.isCrueltyFree == false ? "No" : "Unknown")")
-                        Text("Others: \(data.others)")
+                   
+                    
+                    if  let data = product {
+                       // Text("BARCODE: \(data.barcode)")
+                        // Text("Name: \(data.name)")
+                        // Text("Company: \(data.company)")
+                        // Text("Description: \(data.description)")
+                      
+                        //  Text("Category: \(data.category)")
+                        //  Text("Country: \(data.country)")
+                        // Text("IsCrueltyFree?: \(data.isCrueltyFree == true ? "Yes" : data.isCrueltyFree == false ? "No" : "Unknown")")
+                        // Text("Others: \(data.others)")
                         
                         Button (action:{
                             if(!(favourites.contains(where: {$0.barcode == self.product?.barcode}))){
@@ -93,8 +92,16 @@ struct Scanning: View {
                             
                         })
                         
-                        
-                    } else {
+                        CardScan(prod: data)
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    else {
+                       // data = nil
                         Text("Data not found")
                         
                     }
@@ -105,201 +112,219 @@ struct Scanning: View {
             ScannerView(scannedCode: $scannedCode)
             
         }
-        .sheet(isPresented: $isShowingCard) {
-            CardScan()}
+       // .sheet(isPresented: $isShowingCard) {
+         //   CardScan()
+       // }
         
         .onReceive(Just(scannedCode)) { code in
             guard let code = code else { return }
             fetchDataFromURL(barcode: code)
             isShowingScanner = false
-            isShowingCard = true
             
         }
         
-            
+        
+    }
+    
+ 
+    
+    // Funzione per ottenere i dati dall'URL
+    private func fetchDataFromURL(barcode: String) {
+        let baseURL = "https://myapisrv.obbar.it/api/Product/getProductByBarcode?barcode="
+        
+        guard let url = URL(string: baseURL + barcode) else {
+            print("URL non valido")
+            return
         }
         
-        func decodeBase64ToImage(base64String: String) -> UIImage? {
-            guard let imageData = Data(base64Encoded: base64String) else {
-                return nil
-            }
-            return UIImage(data: imageData)
-        }
-        
-        // Funzione per ottenere i dati dall'URL
-        private func fetchDataFromURL(barcode: String) {
-            let baseURL = "https://myapisrv.obbar.it/api/Product/getProductByBarcode?barcode="
-            
-            guard let url = URL(string: baseURL + barcode) else {
-                print("URL non valido")
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Errore durante il recupero dei dati:", error?.localizedDescription ?? "Errore sconosciuto")
                 return
             }
             
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                guard let data = data, error == nil else {
-                    print("Errore durante il recupero dei dati:", error?.localizedDescription ?? "Errore sconosciuto")
-                    return
-                }
-                
-                do {
-                    // Decodifica dei dati JSON nella struttura Product e aggiornamento della proprietà @State
-                    let decodedData = try JSONDecoder().decode(Product.self, from: data)
-                    DispatchQueue.main.async
-                    {
-                        self.product = decodedData
-                        
-                        //se nella cronologia non è presente il barcode, viene aggiunto una sola volta
-                        if(!(chronology.contains(where: {$0.barcode == self.product?.barcode}))){
-                            chronology.append(self.product!)
-                        }
+            do {
+                // Decodifica dei dati JSON nella struttura Product e aggiornamento della proprietà @State
+                let decodedData = try JSONDecoder().decode(Product.self, from: data)
+                DispatchQueue.main.async
+                {
+                    self.product = decodedData
+                    
+                    //se nella cronologia non è presente il barcode, viene aggiunto una sola volta
+                    if(!(chronology.contains(where: {$0.barcode == self.product?.barcode}))){
+                        chronology.append(self.product!)
                     }
-                } catch {
-                    print("Errore durante la decodifica dei dati:", error)
                 }
-            }
-            
-            task.resume()
-        }
-        
-        
-        
-        
-    }
-    
-    
-    //FUNZIONI PER LA FOTOCAMERA
-    struct ScannerView: UIViewControllerRepresentable {
-        @Binding var scannedCode: String?
-        
-        
-        func makeUIViewController(context: Context) -> ScannerViewController {
-            let scannerViewController = ScannerViewController()
-            scannerViewController.delegate = context.coordinator
-            return scannerViewController
-        }
-        
-        func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {}
-        
-        func makeCoordinator() -> Coordinator {
-            Coordinator(parent: self)
-        }
-        
-        class Coordinator: NSObject, ScannerViewControllerDelegate {
-            var parent: ScannerView
-            
-            init(parent: ScannerView) {
-                self.parent = parent
-            }
-            
-            func didScanCode(_ code: String) {
-                print("Codice a barre scansionato:", code)
-                parent.scannedCode = code
+            } catch {
+                print("Errore durante la decodifica dei dati:", error)
+                self.product=nil
             }
         }
-    }
-    
-    
-    //CARD CHE COMPARE APPENA SCANSIONO
-    
-    struct CardScan : View{
         
-        var body : some View{
-            
-            ZStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.white)
-                    .shadow(radius: 5).frame(width:350,height:120)
-                
-                RoundedRectangle(cornerRadius: 20).foregroundColor(verdeCard) .shadow(radius: 5).frame(width:100,height:100).offset(x:-110,y:0)
-                
-                
-            }
-        }
-    }
-    
-    
-    
-    #Preview{
-        CardScan()
+        task.resume()
     }
     
     
     
     
+}
+
+
+//FUNZIONI PER LA FOTOCAMERA
+struct ScannerView: UIViewControllerRepresentable {
+    @Binding var scannedCode: String?
     
-    //...
     
-    protocol ScannerViewControllerDelegate: AnyObject {
-        func didScanCode(_ code: String)
+    func makeUIViewController(context: Context) -> ScannerViewController {
+        let scannerViewController = ScannerViewController()
+        scannerViewController.delegate = context.coordinator
+        return scannerViewController
     }
     
-    class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
-        weak var delegate: ScannerViewControllerDelegate?
-        var captureSession: AVCaptureSession!
+    func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, ScannerViewControllerDelegate {
+        var parent: ScannerView
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            setupCamera()
+        init(parent: ScannerView) {
+            self.parent = parent
         }
         
-        func setupCamera() {
-            guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-            guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        func didScanCode(_ code: String) {
+            print("Codice a barre scansionato:", code)
+            parent.scannedCode = code
+        }
+    }
+}
+
+
+//CARD CHE COMPARE APPENA SCANSIONO
+
+struct CardScan : View{
+    
+    @State public var prod: Product
+    
+    
+    var body : some View{
+    
+        ZStack{
             
-            captureSession = AVCaptureSession()
-            captureSession.addInput(input)
+           
             
-            let metadataOutput = AVCaptureMetadataOutput()
-            if captureSession.canAddOutput(metadataOutput) {
-                captureSession.addOutput(metadataOutput)
-                
-                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-                metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
-            } else {
-                print("Could not add metadata output")
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.white)
+                .shadow(radius: 5).frame(width:350,height:120)
+            
+            RoundedRectangle(cornerRadius: 20).foregroundColor(verdeCard) .shadow(radius: 5).frame(width:100,height:100).offset(x:-110,y:0)
+            
+            Text(prod.name).foregroundColor(.black).font(.largeTitle)
+            if let decodedImage = self.decodeBase64ToImage(base64String: prod.image) {
+                Image(uiImage: decodedImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .padding()
             }
-            let previewWidth: CGFloat = 400
-            let previewHeight: CGFloat = 600
-            // Calculate the center position of the screen
-            let screenWidth = UIScreen.main.bounds.width
-            let screenHeight = UIScreen.main.bounds.height
-            let centerX = (screenWidth - previewWidth) / 2
-            let centerY = (screenHeight - previewHeight) / 2
-            
-            // Set the frame of the preview layer
-            previewLayer.frame = CGRect(x: centerX, y: centerY, width: previewWidth, height: previewHeight)
-            previewLayer.session = captureSession
-            //previewLayer.frame = view.layer.bounds
-            //previewLayer.videoGravity = .resizeAspectFill
-            view.layer.addSublayer(previewLayer)
-            
-            captureSession.startRunning()
             
             
+        }.background(Color.clear)
+    }
+    
+    
+    
+    
+    
+    func decodeBase64ToImage(base64String: String) -> UIImage? {
+        guard let imageData = Data(base64Encoded: base64String) else {
+            return nil
         }
+        return UIImage(data: imageData)
+    }
+}
+
+
+
+
+
+
+
+//...
+
+protocol ScannerViewControllerDelegate: AnyObject {
+    func didScanCode(_ code: String)
+}
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    weak var delegate: ScannerViewControllerDelegate?
+    var captureSession: AVCaptureSession!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-            if let metadataObject = metadataObjects.first {
-                guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-                guard let stringValue = readableObject.stringValue else { return }
-                
-                captureSession.stopRunning()
-                DispatchQueue.main.async {
-                    // Remove all sublayers including the previewLayer
-                    self.view.layer.sublayers?.forEach { $0.removeFromSuperlayer()
-                        
-                    }
-                    // Inform the delegate of the scanned code
-                    self.delegate?.didScanCode(stringValue)
+        setupCamera()
+    }
+    
+    func setupCamera() {
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession = AVCaptureSession()
+        captureSession.addInput(input)
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        } else {
+            print("Could not add metadata output")
+        }
+        let previewWidth: CGFloat = 400
+        let previewHeight: CGFloat = 600
+        // Calculate the center position of the screen
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        let centerX = (screenWidth - previewWidth) / 2
+        let centerY = (screenHeight - previewHeight) / 2
+        
+        // Set the frame of the preview layer
+        previewLayer.frame = CGRect(x: centerX, y: centerY, width: previewWidth, height: previewHeight)
+        previewLayer.session = captureSession
+        //previewLayer.frame = view.layer.bounds
+        //previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+        
+        
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            
+            captureSession.stopRunning()
+            DispatchQueue.main.async {
+                // Remove all sublayers including the previewLayer
+                self.view.layer.sublayers?.forEach { $0.removeFromSuperlayer()
+                    
                 }
-                
-                // delegate?.didScanCode(stringValue)
+                // Inform the delegate of the scanned code
+                self.delegate?.didScanCode(stringValue)
             }
+            
+            // delegate?.didScanCode(stringValue)
         }
-        
-        
     }
+    
+    
+}
 
 
 
@@ -307,7 +332,7 @@ struct Scanning: View {
 
 
 /*#Preview {
-    Scanning()
-}*/
+ Scanning()
+ }*/
 
 
